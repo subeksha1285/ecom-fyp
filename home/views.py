55035dd00c1fc179ca219199b1e434f62bf7947f
subeksha <subu.chy41285@gmail.com>
2022-03-25 22:13:02 +0545

    Login and register updated

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from ast import Param
 from math import ceil
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from home.models import Product
 from .models import *
+from django.contrib.auth import login, authenticate 
+from django.contrib import messages
+from .forms import UserRegistrationForm
 
 # Create your views here.
 def home(request):
@@ -15,6 +18,19 @@ def viewproduct(request):
     params = {'products' : products}
     return render(request, 'p1.html', params)
 
-def log(request):
-    return render(request, 'log.html')
-                                                                                                                                                                                                                                                                   
+def index(request):
+    return render(request, 'user/index.html', {'title':'index'})
+  
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request,'Your account has been created. You can log in now!')    
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+
+    context = {'form': form}
+    return render(request, 'register.html', context)                                                                                                                                                                                        
